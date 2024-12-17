a997b83327c07452df9ac5a88c5f2f6733fdec7b
Rahulkumartv344 <rahulkumatv9999@gmail.com>
2024-12-17 21:16:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://ccake1764:euHE1dXbnuomypKO@cluster0.oiwbh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = environ.get("DB_URI", "mongodb+srv://ccake1764:jVzHfzd8nTAtjzSE@cluster0.ke2y7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "ccake1764")
 
 # Auto Delete Information
